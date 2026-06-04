35ef4a760a6740b21cb0ff97552c47e40e57be90
Neeraj Pandey <neerajpandey@mac.lan>
2026-06-03 23:37:36 -0500

    allowed ec2 to connect

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    allowedHosts: ["ec2-3-81-6-27.compute-1.amazonaws.com"],
     proxy: {
       "/api": "http://localhost:8001",
     },
